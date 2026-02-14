c548065681bbfc26caf0e28ca24630ecacf39c04
SebasF10 <sebastain.f.murillo@gmail.com>
2026-02-14 09:23:49 -0500

    Se empezo a enlazar la logica

--- menuCoordinador.py
+++ menuCoordinador.py
@@ -4,5 +4,7 @@ import os
 def menuCoordinador():
     while True:
         print("Bienvenido que es lo que quieres hacer?")
-        print("1. Ver lista de campers")
+        print("1. Agendar estudiantes a evaluacion de ingreso")
+        print("2. Estudiantes para evalucion de ingreso")
+        print("3. Asignar grupos a estudiantes Activos")
         
